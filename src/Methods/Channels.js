09df487b27e3ab9aceb3cb4723a2a81a640e809b
YoussefElshemi <33766341+YoussefElshemi@users.noreply.github.com>
2018-03-25 23:44:43 +0100

    New Channel Methods
    Added, lots of methods to edit the channels
    TODO: Add checks for the edits and type of channel etc. if the channel isn't a textchannel, but they're calling .send on it, create a new error type

--- src/Methods/Channels.js
+++ src/Methods/Channels.js
@@ -113,6 +113,142 @@ module.exports = function() {
         });
       };
 
+      /**
+       * @description Sets the name of the channel
+       * @param {String} newname The name of the channel
+       * @returns {Promise<GuildChannel>} Returns a promise and a Guild Channel
+       */
+
+      raw.setName = function(newname) {
+        return new Promise((res) => {
+          request.req("PATCH", `/channels/${raw.id}`, {
+            name: newname
+          }, _this.token).then(m => {
+            setTimeout(res, 100, res(_this.channel_methods().fromRaw(m)));
+          });
+        });
+      };
+
+      /**
+       * @description Sets the position of the channel
+       * @param {Number} position The position of the channel
+       * @returns {Promise<GuildChannel>} Returns a promise and a Guild Channel
+       */
+
+      raw.setPosition = function(position) {
+        return new Promise((res) => {
+          request.req("PATCH", `/channels/${raw.id}`, {
+            osition: Number(position)
+          }, _this.token).then(m => {
+            setTimeout(res, 100, res(_this.channel_methods().fromRaw(m)));
+          });
+        });
+      };
+
+      /**
+       * @description Sets the topic of the channel
+       * @param {String} newtopic The topic of the channel
+       * @returns {Promise<GuildChannel>} Returns a promise and a Guild Channel
+       */
+
+      raw.setTopic = function(newtopic) {
+        return new Promise((res) => {
+          request.req("PATCH", `/channels/${raw.id}`, {
+            topic: newtopic
+          }, _this.token).then(m => {
+            setTimeout(res, 100, res(_this.channel_methods().fromRaw(m)));
+          });
+        });
+      };
+
+      /**
+       * @description Sets the nsfw of the channel
+       * @param {Boolean} falseortrue Whether the channel should be nsfw or not
+       * @returns {Promise<GuildChannel>} Returns a promise and a Guild Channel
+       */
+
+      raw.setNSFW = function(falseortrue) {
+        return new Promise((res) => {
+          request.req("PATCH", `/channels/${raw.id}`, {
+            nsfw: falseortrue
+          }, _this.token).then(m => {
+            setTimeout(res, 100, res(_this.channel_methods().fromRaw(m)));
+          });
+        });
+      };
+
+      /**
+       * @description Sets the bitrate of the channel
+       * @param {Number} bitrate The bitrate of the channel
+       * @returns {Promise<GuildChannel>} Returns a promise and a Guild Channel
+       */
+
+      raw.setBitrate = function(bitrate) {
+        return new Promise((res) => {
+          request.req("PATCH", `/channels/${raw.id}`, {
+            bitrate: bitrate
+          }, _this.token).then(m => {
+            setTimeout(res, 100, res(_this.channel_methods().fromRaw(m)));
+          });
+        });
+      };
+
+      /**
+       * @description Sets the user limit of the channel
+       * @param {Number} limit The user limit of the channel
+       * @returns {Promise<GuildChannel>} Returns a promise and a Guild Channel
+       */
+
+      raw.setUserLimit = function(limit) {
+        return new Promise((res) => {
+          request.req("PATCH", `/channels/${raw.id}`, {
+            user_limit: Number(limit)
+          }, _this.token).then(m => {
+            setTimeout(res, 100, res(_this.channel_methods().fromRaw(m)));
+          });
+        });
+      };
+
+      /**
+       * @description Sets the parent of the channel
+       * @param {Snowflake} setParent The id of the parent channel
+       * @returns {Promise<GuildChannel>} Returns a promise and a Guild Channel
+       */
+
+      raw.setParent = function(newparent) {
+        return new Promise((res) => {
+          request.req("PATCH", `/channels/${raw.id}`, {
+            parent_id: newparent
+          }, _this.token).then(m => {
+            setTimeout(res, 100, res(_this.channel_methods().fromRaw(m)));
+          });
+        });
+      };
+
+      /**
+       * @description Edits the channel 
+       * @param {Object} options Available options: name, position, topic, nsfw, bitrate, userlimit and parent, similar to above methods
+       * @returns {Promise<GuildChannel>} Returns a promise and a Guild Channel
+       */
+
+      raw.edit = function(options) {
+        return new Promise((res) => {
+          request.req("POST", `/guilds/${raw.id}/channels`, 
+            {
+              name: (options && options.name) || null,
+              position: (options && options.position) || null,
+              topic: (raw.type === "text" && options && options.topic) || null,
+              nsfw: (raw.type === "text" && options && options.nsfw) || null,
+              bitrate: (raw.type === "voice" && options && options.bitrate) || null,
+              user_limit: (raw.type === "voice" && options && options.userlimit) || null,
+              parent_id: ((raw.type === "text" || raw.type === "voice") && options && options.parent) || null  
+            }, _this.token)
+            .then(m => {
+              setTimeout(res, 100, res(_this.channel_methods().fromRaw(m)));  
+            });
+        });
+      };
+
       return raw;
     }
   };
