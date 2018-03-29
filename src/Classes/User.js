e065237be71d47ab0119e4816f3bc67dc072e07f
Unknown <youssefelshemi@gmail.com>
2018-03-29 21:01:09 +0100

    User Class
    Added some properties

--- src/Classes/User.js
+++ src/Classes/User.js
@@ -4,6 +4,19 @@
  */
 
 class User {
+  constructor(raw) {
+
+    this.id = raw.id;
+
+    this.username = raw.username;
+
+    this.discriminator = raw.discriminator;
+
+    this.tag = raw.tag;
+
+    this.avatarURL = `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.png`;
+      
+  }
 
 }
 
