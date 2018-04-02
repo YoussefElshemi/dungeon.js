1277a75495a0e790f34cdfededb51f83d0b0c2b7
Top Dog <topbubble396@gmail.com>
2018-04-02 20:24:31 +0100

    Added permissions, put into ./index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   Client: require('./src/Classes/Client'),
   Embed: require('./src//Classes/Embed'),
   Collection: require('./src/Classes/Collection'),
+  Permissions: require('./src/Classes/Permissions'),
   Message: require('./src/Classes/Message'),
   Guild: require('./src/Classes/Guild')
 };
