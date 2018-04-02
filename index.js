3c550df09cacaecf4af943f96ca70f8e6fcae494
Top Dog <topbubble396@gmail.com>
2018-04-02 20:22:16 +0100

    Revert "Added permissions class and put onto ./index.js"
    This reverts commit 0fbbaf980785ae9478b62b9f1d996ae6de74cca7.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 module.exports = {
   Client: require('./src/Classes/Client'),
-  Embed: require('./src/Classes/Embed'),
+  Embed: require('./src//Classes/Embed'),
   Collection: require('./src/Classes/Collection'),
-  Permissions: require('./src/Classes/Permissions'),
   Message: require('./src/Classes/Message'),
   Guild: require('./src/Classes/Guild')
 };
