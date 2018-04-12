c8ee41c5fc72b9b9e82e6f5c46c2a2410321b852
Top Dog <topbubble396@gmail.com>
2018-04-12 18:05:23 +0100

    getPinned() pinMessage() removePinned()
    - Attempt at documentating too.

--- src/Classes/GuildChannel.js
+++ src/Classes/GuildChannel.js
@@ -11,7 +11,7 @@ class GuildChannel {
      * The ID of the channel
      * @type {String}
      */
-    
+
     this.id = raw.id;
 
     /**
@@ -90,7 +90,7 @@ class GuildChannel {
         const VoiceChannel = require('./VoiceChannel');
         if (this.type === 'text') return setTimeout(res, 100, res(new TextChannel(c, this.client)));
         if (this.type === 'voice') return setTimeout(res, 100, res(new VoiceChannel(c, this.client)));
-        setTimeout(res, 100, res(new GuildChannel(m, this.client)));  
+        setTimeout(res, 100, res(new GuildChannel(m, this.client)));
       });
     });
   }
@@ -114,7 +114,7 @@ class GuildChannel {
         const VoiceChannel = require('./VoiceChannel');
         if (this.type === 'text') return setTimeout(res, 100, res(new TextChannel(c, this.client)));
         if (this.type === 'voice') return setTimeout(res, 100, res(new VoiceChannel(c, this.client)));
-        setTimeout(res, 100, res(new GuildChannel(m, this.client)));    
+        setTimeout(res, 100, res(new GuildChannel(m, this.client)));
       });
     });
   }
@@ -133,20 +133,20 @@ class GuildChannel {
           const VoiceChannel = require('./VoiceChannel');
           if (this.type === 'text') return setTimeout(res, 100, res(new TextChannel(c, this.client)));
           if (this.type === 'voice') return setTimeout(res, 100, res(new VoiceChannel(c, this.client)));
-          setTimeout(res, 100, res(new GuildChannel(m, this.client)));      
+          setTimeout(res, 100, res(new GuildChannel(m, this.client)));
         });
     });
   }
 
   /**
-   * @description Edits the channel 
+   * @description Edits the channel
    * @param {Object} options Available options: name, position, topic, nsfw, bitrate, userlimit and parent, similar to above methods
    * @returns {Promise<GuildChannel>} Returns a promise and a Guild Channel
    */
 
   edit(options) {
     return new Promise((res) => {
-      request.req('POST', `/guilds/${this.id}/channels`, 
+      request.req('POST', `/guilds/${this.id}/channels`,
         {
           name: (options && options.name) || null,
           position: (options && options.position) || null,
@@ -154,7 +154,7 @@ class GuildChannel {
           nsfw: (this.type === 'text' && options && options.nsfw) || null,
           bitrate: (this.type === 'voice' && options && options.bitrate) || null,
           user_limit: (this.type === 'voice' && options && options.userlimit) || null,
-          parent_id: ((this.type === 'text' || this.type === 'voice') && options && options.parent) || null  
+          parent_id: ((this.type === 'text' || this.type === 'voice') && options && options.parent) || null
         }, this.client.token)
         .then(m => {
           const GuildChannel = require('./GuildChannel');
@@ -162,8 +162,33 @@ class GuildChannel {
           const VoiceChannel = require('./VoiceChannel');
           if (this.type === 'text') return setTimeout(res, 100, res(new TextChannel(c, this.client)));
           if (this.type === 'voice') return setTimeout(res, 100, res(new VoiceChannel(c, this.client)));
-          setTimeout(res, 100, res(new GuildChannel(m, this.client)));   
+          setTimeout(res, 100, res(new GuildChannel(m, this.client)));
         });
+    });
+  }
+
+  /**
+    @description Edit permissions of the channels
+    @param {String} rm Available options: Role or Member.
+    @param {Snowflake} id The ID for the role or member.
+    @param {Object} opt Available options: Allow (Bitfield of Permissions) and Deny (Bitfield of Permissions)
+    */
+
+  editPermissions(rm, id, opt) { /* SUBJECT TO CHANGE */
+    const opt = opt || {};
+
+    return new Promise((res) => {
+      request.req('PUT', `/channels/${this.id}/permissions/${id}`, {
+        allow: opt.allow || 0,
+        deny: opt.deny || 0,
+        type: rm
+      }, this.client.token).then(success => {
+        request.req('GET', `/channels/${this.id}`, {}, this.client.token).then(channel => {
+          setTImeout(res, 100, res(new this.constructor(channel, this.client)));
+        });
+      }).catch(error => {
+        if (error.status === 403) throw new this.client.MissingPermissions('I don\'t have permissions to perform this action!');
+      });
     });
   }
 }
@@ -172,7 +197,7 @@ class GuildChannel {
  * The options for making/editing a channel
  * @typedef {Object} ChannelOptions
  * @property {String} [name = null] The name of the channel
- * @property {Number} [position = null] Position of the channel 
+ * @property {Number} [position = null] Position of the channel
  * @property {String} [topic = null] The topic of the channel (textchannel only)
  * @property {Boolean} [nsfw = null] If the channel is NSFW or not (textchannel only)
  * @property {Number} [bitrate = null] The bitrate of the channel (voicechannel only)
